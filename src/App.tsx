9f1b2d9aed9b329baa41c4212f582dac8c2b0afa
Yaroslav Sulaev <yarsul777891011@gmail.com>
2024-05-24 20:38:40 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Coming soon...
+        </h2>
+      </header>
+    </div>
+  );
+}
+
